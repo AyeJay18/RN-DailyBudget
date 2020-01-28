df4742b605b84fc8fb1f287252d3d14fabe2aa1a
AJ Hall <ajhall@AJs-MacBook-Pro.local>
2020-01-28 15:26:36 -0600

    First Commit

---
+++ App.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text>Look at me making apps and stuff!</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
